9d8ec3051102cf9ae504b1fdd2718276f8527ef7
AlexGegen <55158286+AlexGegen@users.noreply.github.com>
2022-10-10 11:15:40 +0200

    Changed Some Stuff

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--img alt="Vue logo" src="https://vuejs.org/images/logo.png"-->
+    <!--HelloWorld msg="Good Morning"/-->
+    <h1>Good Morning 5AHIT</h1>
   </div>
 </template>
 
